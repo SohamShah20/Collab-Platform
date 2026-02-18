6c360271e36f676616bd959c01791f6641602f27
SohamShah20 <sohamjshah20@gmail.com>
2026-02-18 14:42:18 +0530

    Create authRoutes.js

---
+++ backend/routes/authRoutes.js
@@ -0,0 +1,74 @@
+const express = require("express")
+const bcrypt = require("bcryptjs")
+const jwt = require("jsonwebtoken")
+const User = require("../models/User")
+
+const router = express.Router()
+
+const generateToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: "7d"
+  })
+}
+
+router.post("/register", async (req, res) => {
+    try{
+        const {name, email, password} = req.body
+        if(!name || !email || !password){
+            return res.status(400).json({message: "All fields are required"})
+        }
+        
+        const existingUser = await User.findOne({email})
+        if(existingUser){
+            return res.status(400).json({message: "User already exists"})
+        }
+
+        const hashedPassword = await bcrypt.hash(password, 10)
+
+        const user = await User.create({
+            name,
+            email,
+            password: hashedPassword
+        })
+
+        const token = generateToken(user._id)
+
+        res.cookie("token", token, {
+            httpOnly: true,
+            secure: false,
+            sameSite: "lax"
+        })
+
+        res.json({
+            id: user._id,
+            name: user.name,
+            email: user.email
+        })
+
+    } catch(err){
+        res.status(500).json({message: "Server error"})
+    }
+})
+
+router.post("/logout", (req, res) => {
+    res.clearCookie("token")
+    res.json({message: "Logged out"})
+})
+
+router.get("/me", async (req, res) => {
+    try{
+        const token = req.cookies.token
+        if(!token){
+            return res.status(401).json({message: "Not authorized"})
+        }
+
+        const decoded = jwt.verify(token, process.env.JWT_SECRET)
+        const user = await User.findById(decoded.id).select("-password")
+
+        res.json(user)
+    }catch{
+        res.status(401).json({message: "Not authorized"})
+    }
+})
+
+module.exports = router
